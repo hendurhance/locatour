5190296b305b4be5c981beb86407f889a8cec5c1
hendurhance <50846992+hendurhance@users.noreply.github.com>
2021-02-22 13:13:20 +0100

    working on distance matrix api request

--- src/components/OriginDestination.vue
+++ src/components/OriginDestination.vue
@@ -14,18 +14,35 @@
           <input type="text" placeholder="Destination" ref="destination">
         </div>
       </div>
-      <button class="ui button small green">Routes</button>
+      <button class="ui button small green" @click="calculateRoutes">Routes</button>
     </div>
   </div>
 </section>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  data(){
+    return{
+      route: {
+        origin: {
+          address: '',
+          lat: 0,
+          lng: 0
+        },
+        destination: {
+          address: '',
+          lat: 0,
+          lng: 0
+        }
+      }
+    }
+  },
   mounted(){
-    // const google = window.google
     for(let ref in this.$refs){
-      console.log(this.$refs[ref])
+      // console.log(this.$refs[ref])
       const google = window.google
       const autocomplete = new google.maps.places.Autocomplete(
         this.$refs[ref],
@@ -37,7 +54,24 @@ export default {
       )
 
       autocomplete.addListener('place_changed', () => {
-        console.log(autocomplete.getPlace())
+        const place = autocomplete.getPlace()
+        this.route[ref].address = `${place.name}, ${place.vicinity}`
+        this.route[ref].lat = place.geometry.location.lat()
+        this.route[ref].lng = place.geometry.location.lng()
+      })
+    }
+  },
+  methods: {
+    calculateRoutes(){
+      console.log(this.route)
+      const URL = `https://secret-ocean-49799.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?origins=${this.route.origin.lat},${this.route.origin.lng}&destinations=${this.route.destination.lat},${this.route.destination.lng}&key=AIzaSyC_PNlFvbM1LVO55Ep_8NKyf6oylut7JTg`
+      axios
+      .get(URL)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error.message)
       })
     }
   }
