17c03462d924781cd232c086dbe2ebfc23d2594b
hendurhance <50846992+hendurhance@users.noreply.github.com>
2021-02-23 19:11:28 +0100

    sorting Cloud FireStore data

--- src/components/RouteList.vue
+++ src/components/RouteList.vue
@@ -2,6 +2,17 @@
 <section class="route-list">
   <div class="route-list-header">
     <h3 class="ui header">Route List</h3>
+    <select @change="sortRoute($event)">
+      <option selected disabled>Sort</option>
+      <optgroup label="Distance">
+        <option value="distance-asc">short</option>
+        <option value="distance-desc">long</option>
+      </optgroup>
+      <optgroup label="Duration">
+        <option value="duration-asc">fast</option>
+        <option value="duration-desc">slow</option>
+      </optgroup>
+    </select>
   </div>
   <div class="item" v-for="route in routes" :key="route.id">
     <div>
@@ -39,6 +50,24 @@ export default {
         })
       }
     )
+  },
+  methods: {
+    sortRoute(e){
+      const sortName = e.target.value.split('-')[0]
+      const sortOrder = e.target.value.split('-').[1]
+      
+      const db = firebase.firestore()
+      db.collection('routes').orderBy(sortName + '.value', sortOrder)
+      .get()
+      .then(snap => {
+        this.routes = []
+        snap.forEach(doc => {
+          let route = doc.data()
+          route.id = doc.id
+          this.routes.push(route)
+        })
+      })
+    }
   }
 }
 </script>
