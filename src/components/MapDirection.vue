35c93e169a399f8e33c54a28dc7d0bb0dcead05f
hendurhance <50846992+hendurhance@users.noreply.github.com>
2021-02-24 21:17:00 +0100

    working on marking on two direction

--- src/components/MapDirection.vue
+++ src/components/MapDirection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="map">
+  <section class="map" ref="map">
 
   </section>
 </template>
@@ -8,8 +8,37 @@
 import { EventBus } from '@/EventBus.js'
 export default {
   mounted(){
-    EventBus.$on('route-data', data => {
-      console.log(data)
+    // Google constant
+    const google = window.google
+
+    const map = new google.maps.Map(
+      this.$refs['map'], {
+        center: new google.maps.LatLng(6.5244, 3.3792),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+    )
+    const directionsService = new google.maps.DirectionsService()
+    const directionsRenderer = new google.maps.DirectionsRenderer()
+
+    // console.log('map info', map)
+
+    EventBus.$on('route-data', ({origin, destination}) => {
+      directionsService.route(
+        {
+          origin: origin.address,
+          destination: destination.address,
+          travelMode: 'DRIVING'
+        },
+        (response, status) => {
+          if(status === 'OK'){
+            directionsRenderer.setDirections(response)
+            directionsRenderer.setMap(map)
+          }
+          console.log(response)
+          console.warn(status)
+        }
+      )
     })
 
   }
